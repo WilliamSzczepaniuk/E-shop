a0655168d3e01ef55bba1a3b0204a4fb7f98c82d
williamszczepaniuk <williamng4@outlook.com>
2022-12-12 09:47:54 -0300

    Fix:InputSearch-width

--- src/Components/Header/InputSearch.jsx
+++ src/Components/Header/InputSearch.jsx
@@ -7,55 +7,66 @@ import { Box } from "@mui/system";
 import { alpha } from "@mui/material/styles";
 
 const useStyles = makeStyles((theme) => ({
+  BoxPrimary: {
+    position: "relative",
+    width: "100%",
+    backgroundColor: alpha(theme.palette.common.white, 0.2),
+    [theme.breakpoints.up("sm")]: {
+      padding: theme.spacing(1),
+      width: "100%",
+    },
+    marginLeft: 0,
+    marginRight: 0,
+    borderRadius: theme.shape.borderRadius,
+    [theme.breakpoints.up("xs")]: {
+      marginLeft: theme.spacing(1),
+      width: "auto",
+    },
+    display: "flex",
+    justifyContent: "end",
+  },
+  boxIcon: {
+    [theme.breakpoints.up("xs")]: {
+      marginRight: theme.spacing(1),
+      width: "auto",
+    },
+    marginTop: "10px",
+    position: "relative",
+    maxWidth: "300px",
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
   input: {
-    color: "inherit",
+    borderRadius: "20px",
+    boxShadow: "10px 5px 10px 3px rgba(244, 204, 255, 1)",
+    "& input::placeholder": { color: "black", fontSize: "18px" },
     "& .MuiInputBase-input": {
       padding: theme.spacing(1, 1, 1, 0),
-      paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+      paddingLeft: `calc(1.3em + ${theme.spacing(4)}px)`,
       transition: theme.transitions.create("width"),
       width: "100%",
-      border: "red 1px solid",
       [theme.breakpoints.up("sm")]: {
         width: "16ch",
         "&:focus": {
-          width: "28ch",
+          width: "26ch",
         },
       },
-    },
-    search: {
-      backgroundColor: alpha(theme.palette.common.white, 0.2),
-      "&:hover": {
-        backgroundColor: alpha(theme.palette.common.white, 0.25),
-      },
-      position: "relative",
-      [theme.breakpoints.up("sm")]: {
-        padding: theme.spacing(2),
-        width: "20%",
-      },
-      marginLeft: 0,
-      marginRight: 10,
-      borderRadius: theme.shape.borderRadius,
-      [theme.breakpoints.up("sm")]: {
-        marginLeft: theme.spacing(1),
-        width: "auto",
-      },
-    },
-    searchIcon: {
-      padding: theme.spacing(0, 2),
-      left: 200,
-      height: "100%",
-      position: "absolute",
-      pointerEvents: "none",
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "center",
     },
   },
 }));
 export const InputSearch = () => {
   const { products, changeProducts, onSearch, setOnSearch } =
     useContext(ProductsContext);
+
   const [inputValue, setInputValue] = useState("");
+
   const classes = useStyles();
 
   const handleChange = (event) => {
@@ -76,26 +87,22 @@ export const InputSearch = () => {
     return changeProducts(searchValue);
   };
   return (
-    <InputBase
-      color="info"
-      value={inputValue}
-      onKeyUp={(e) => handleChange(e)}
-      onChange={(e) => {
-        setInputValue(e.target.value);
-      }}
-      // sx={{
-      //   color: "inherit",
-      //   boxShadow: onSearch ? "0px 4px 7px -4px #000000" : "none",
-      // }}
-      inputProps={{ "aria-label": "search" }}
-      placeholder="Search..."
-      className={classes.input}
-    >
-      <Box className={classes.search}>
+    <Box className={classes.BoxPrimary}>
+      <Box className={classes.boxIcon}>
         <Box className={classes.searchIcon}>
           <SearchIcon />
         </Box>
+        <InputBase
+          value={inputValue}
+          onKeyUp={(e) => handleChange(e)}
+          onChange={(e) => {
+            setInputValue(e.target.value);
+          }}
+          inputProps={{ "aria-label": "search" }}
+          placeholder="Search..."
+          className={classes.input}
+        />
       </Box>
-    </InputBase>
+    </Box>
   );
 };
